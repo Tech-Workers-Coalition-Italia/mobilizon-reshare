821f2a64359c3ff8737989126be4631f2a1f056f
Simone Robutti <simone.robutti@protonmail.com>
2021-05-04 11:48:54 +0200

    added jinja

--- mobilizon_bots/event/event.py
+++ mobilizon_bots/event/event.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, asdict
 from datetime import datetime
 from typing import Optional
+from jinja2 import Template
 
 
 @dataclass
@@ -17,8 +18,8 @@ class MobilizonEvent:
     thumbnail_link: Optional[str] = None
     location: Optional[str] = None
 
-    def _fill_template(self, pattern: str) -> str:
-        return pattern.format(**asdict(self))
+    def _fill_template(self, pattern: Template) -> str:
+        return pattern.render(**asdict(self))
 
-    def format(self, pattern: str) -> str:
+    def format(self, pattern: Template) -> str:
         return self._fill_template(pattern)
--- tests/event/test_event.py
+++ tests/event/test_event.py
@@ -1,6 +1,8 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 
 import pytest
+from jinja2 import Template
+
 from mobilizon_bots.event.event import MobilizonEvent
 
 
@@ -9,8 +11,8 @@ def event() -> MobilizonEvent:
     return MobilizonEvent(
         name="test event",
         description="description of the event",
-        begin_datetime=datetime.now() + timedelta(days=1),
-        end_datetime=datetime.now() + timedelta(days=1, hours=2),
+        begin_datetime=datetime(year=2021, month=1, day=1, hour=11, minute=30),
+        end_datetime=datetime(year=2021, month=1, day=1, hour=12, minute=30),
         last_accessed=datetime.now(),
         mobilizon_link="http://some_link.com/123",
         mobilizon_id="12345",
@@ -19,15 +21,22 @@ def event() -> MobilizonEvent:
     )
 
 
-def test_fill_template(event):
-    assert (
-        event._fill_template("{name}|{description}|{location}")
-        == "test event|description of the event|location"
+@pytest.fixture()
+def simple_template():
+    return Template(
+        "{{name}}|{{description}}|{{location}}|{{begin_datetime.strftime('%Y-%m-%d, %H:%M')}}"
     )
 
 
-def test_format(event):
+def test_fill_template(event, simple_template):
     assert (
-        event.format("{name}|{description}|{location}")
-        == "test event|description of the event|location"
+        event._fill_template(simple_template)
+        == "test event|description of the event|location|2021-01-01, 11:30"
+    )
+
+
+def test_format(event, simple_template):
+    assert (
+        event.format(simple_template)
+        == "test event|description of the event|location|2021-01-01, 11:30"
     )
