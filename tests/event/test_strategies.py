214e2a0c3b43c51cc90fafa927521d81552a4345
Simone Robutti <simone.robutti@protonmail.com>
2021-05-05 14:37:19 +0200

    added more tests

--- tests/event/test_strategies.py
+++ tests/event/test_strategies.py
@@ -36,9 +36,9 @@ def test_window_simple_no_event(
     "desired_break_window,days_passed_from_publication", [[1, 2], [2, 10], [4, 4]]
 )
 def test_window_simple_event_found(
-    event_generator, desired_break_window, days_passed_from_publication
+    event_generator, desired_break_window, days_passed_from_publication,
 ):
-    "Testing that the break between events is respected"
+    "Testing that the break between events is respected and an event is found"
     unpublished_events = [
         event_generator(
             published=False,
@@ -51,6 +51,51 @@ def test_window_simple_event_found(
             publication_time=datetime.now()
             - timedelta(days=days_passed_from_publication),
         )
+    ]
+
+    selected_event = SelectNextEventStrategy(
+        minimum_break_between_events=timedelta(days=desired_break_window)
+    ).select(published_events, unpublished_events)
+    assert selected_event is unpublished_events[0]
+
+
+@pytest.mark.parametrize(
+    "desired_break_window,days_passed_from_publication", [[1, 2], [2, 10], [4, 4]]
+)
+def test_window_multi_event_found(
+    event_generator, desired_break_window, days_passed_from_publication,
+):
+    "Testing that the break between events is respected when there are multiple events"
+    unpublished_events = [
+        event_generator(
+            published=False,
+            begin_date=datetime(year=2022, month=1, day=5, hour=11, minute=30),
+        ),
+        event_generator(
+            published=False,
+            begin_date=datetime(year=2022, month=3, day=5, hour=11, minute=30),
+        ),
+        event_generator(
+            published=False,
+            begin_date=datetime(year=2021, month=1, day=5, hour=11, minute=30),
+        ),
+    ]
+    published_events = [
+        event_generator(
+            published=True,
+            publication_time=datetime.now()
+            - timedelta(days=days_passed_from_publication),
+        ),
+        event_generator(
+            published=True,
+            publication_time=datetime.now()
+            - timedelta(days=days_passed_from_publication + 2),
+        ),
+        event_generator(
+            published=True,
+            publication_time=datetime.now()
+            - timedelta(days=days_passed_from_publication + 4),
+        ),
     ]
 
     selected_event = SelectNextEventStrategy(
