10c6b62a30e932c4cd459ab89de80a9d599326fc
SlyK182 <gregorini.silvio@gmail.com>
2021-05-09 17:25:29 +0200

    Added proper exceptions for publishers

---
+++ mobilizon_bots/publishers/exceptions.py
@@ -0,0 +1,26 @@
+class PublisherError(Exception):
+    """ Generic publisher error """
+
+    def __init__(self, message):
+        """ :param str message: exception message """
+        super().__init__(message)
+
+
+class InvalidBot(PublisherError):
+    """ Publisher refers to the wrong service bot """
+
+
+class InvalidCredentials(PublisherError):
+    """ Publisher cannot validate credentials """
+
+
+class InvalidEvent(PublisherError):
+    """ Publisher cannot validate events """
+
+
+class InvalidMessage(PublisherError):
+    """ Publisher cannot validate message """
+
+
+class InvalidResponse(PublisherError):
+    """ Publisher receives an invalid response from its service """
