ed2d016715b760636fbba5820a90b7cf7654e34f
Simone Robutti <simone.robutti@protonmail.com>
2021-05-09 17:22:14 +0200

    added notifier config

--- mobilizon_bots/config/config.py
+++ mobilizon_bots/config/config.py
@@ -1,6 +1,7 @@
 from dynaconf import Dynaconf, Validator
 
-from mobilizon_bots.config import strategies, publishers
+from mobilizon_bots.config import strategies, publishers, notifiers
+from mobilizon_bots.config.notifiers import notifier_names
 from mobilizon_bots.config.publishers import publisher_names
 
 SETTINGS_FILE = [
@@ -10,10 +11,19 @@ SETTINGS_FILE = [
     "/etc/mobilizon_bots_secrets.toml",
 ]
 ENVVAR_PREFIX = "MOBILIZON_BOTS"
-base_validators = [Validator("selection.strategy", must_exist=True)] + [
-    Validator(f"publisher.{publisher_name}.active", must_exist=True, is_type_of=bool)
-    for publisher_name in publisher_names
-]
+base_validators = (
+    [Validator("selection.strategy", must_exist=True)]
+    + [
+        Validator(
+            f"publisher.{publisher_name}.active", must_exist=True, is_type_of=bool
+        )
+        for publisher_name in publisher_names
+    ]
+    + [
+        Validator(f"notifier.{notifier_name}.active", must_exist=True, is_type_of=bool)
+        for notifier_name in notifier_names
+    ]
+)
 
 raw_settings = Dynaconf(
     environments=True,
@@ -24,12 +34,16 @@ raw_settings = Dynaconf(
 
 strategy_validators = strategies.get_validators(raw_settings)
 publisher_validators = publishers.get_validators(raw_settings)
+notifier_validators = notifiers.get_validators(raw_settings)
 
 settings = Dynaconf(
     environments=True,
     envvar_prefix=ENVVAR_PREFIX,
     settings_files=SETTINGS_FILE,
-    validators=base_validators + strategy_validators + publisher_validators,
+    validators=base_validators
+    + strategy_validators
+    + publisher_validators
+    + notifier_validators,
 )
 # TODO use validation control in dynaconf 3.2.0 once released
 settings.validators.validate()
