a6ae4ceda085d0cac9d98a175c3fe5bb8bfc2f28
Giacomo Leidi <goodoldpaul@autistici.org>
2021-06-21 00:49:54 +0200

    storage: query: Add events_with_status.

--- mobilizon_bots/storage/query.py
+++ mobilizon_bots/storage/query.py
@@ -6,19 +6,25 @@ from mobilizon_bots.models.publication import PublicationStatus
 from mobilizon_bots.models.publisher import Publisher
 
 
-async def get_published_events() -> Iterable[MobilizonEvent]:
+async def events_with_status(statuses: list[PublicationStatus]) -> Iterable[MobilizonEvent]:
     return map(
         MobilizonEvent.from_model,
         await Event.filter(
-            publications__status__in=[
-                PublicationStatus.COMPLETED,
-                PublicationStatus.PARTIAL,
-            ]
+            publications__status__in=statuses
         )
+        .prefetch_related("publications")
+        .prefetch_related("publications__publisher")
         .order_by("begin_datetime")
-        .distinct(),
+        .distinct()
     )
 
 
-async def create_publisher(name: str, account_ref: Optional[str] = None):
+async def get_published_events() -> Iterable[MobilizonEvent]:
+    return await events_with_status([
+            PublicationStatus.COMPLETED,
+            PublicationStatus.PARTIAL,
+        ])
+
+
+async def create_publisher(name: str, account_ref: Optional[str] = None) -> None:
     await Publisher.create(name=name, account_ref=account_ref)
