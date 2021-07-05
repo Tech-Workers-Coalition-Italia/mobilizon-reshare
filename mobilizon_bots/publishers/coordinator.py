587960e3b6f4acb93471f5e93fe960988d886e2e
SlyK182 <gregorini.silvio@gmail.com>
2021-07-05 23:12:10 +0200

    PublisherCoordinatorResult: status turned from str to PublicationStatus

--- mobilizon_bots/publishers/coordinator.py
+++ mobilizon_bots/publishers/coordinator.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 
 from mobilizon_bots.config.config import settings
 from mobilizon_bots.config.publishers import get_active_publishers
-from mobilizon_bots.event.event import MobilizonEvent
+from mobilizon_bots.event.event import MobilizonEvent, PublicationStatus
 from .exceptions import PublisherError
 from .telegram import TelegramPublisher
 
@@ -11,7 +11,7 @@ KEY2CLS = {"telegram": TelegramPublisher}
 
 @dataclass
 class PublisherCoordinatorResult:
-    status: str
+    status: PublicationStatus
     description: str
     invalid_credentials_publishers: list = field(default_factory=[])
     invalid_event_publishers: list = field(default_factory=[])
@@ -30,7 +30,7 @@ class PublisherCoordinator:
         invalid_credentials, invalid_event, invalid_msg = self._validate()
         if invalid_credentials or invalid_event or invalid_msg:
             return PublisherCoordinatorResult(
-                status="fail",
+                status=PublicationStatus.FAILED,
                 description="Validation failed for at least 1 publisher",
                 invalid_credentials_publishers=invalid_credentials,
                 invalid_event_publishers=invalid_event,
@@ -40,14 +40,14 @@ class PublisherCoordinator:
         failed_publishers, successful_publishers = self._post()
         if failed_publishers:
             return PublisherCoordinatorResult(
-                status="fail",
+                status=PublicationStatus.FAILED,
                 description="Posting failed for at least 1 publisher",
                 failed_publishers=failed_publishers,
                 successful_publishers=successful_publishers,
             )
 
         return PublisherCoordinatorResult(
-            status="success",
+            status=PublicationStatus.COMPLETED,
             description="https://www.youtube.com/watch?v=2lHgmC6PBBE",
         )
 
