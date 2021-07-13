c3337cdf87dc20bc8d11af52f1096d0faa11af87
Simone Robutti <simone.robutti@protonmail.com>
2021-07-13 17:11:40 +0200

    fixed teardown

--- mobilizon_bots/main.py
+++ mobilizon_bots/main.py
@@ -2,15 +2,19 @@ import logging.config
 from pathlib import Path
 
 from mobilizon_bots.config.config import update_settings_files
-
 from mobilizon_bots.event.event_selection_strategies import select_event_to_publish
 from mobilizon_bots.mobilizon.events import get_unpublished_events
 from mobilizon_bots.publishers import get_active_publishers
 from mobilizon_bots.publishers.coordinator import PublisherCoordinator
-from mobilizon_bots.storage.db import MobilizonBotsDB
+from mobilizon_bots.storage.db import MobilizonBotsDB, tear_down
 from mobilizon_bots.storage.query import get_published_events, create_unpublished_events
 
 logger = logging.getLogger(__name__)
+
+
+async def graceful_exit(code):
+    await tear_down()
+    exit(code)
 
 
 async def main(settings_file):
@@ -39,7 +43,8 @@ async def main(settings_file):
         result = PublisherCoordinator(event).run()
 
         logger.debug("Closing")
-        exit(0 if result.successful else 1)
+
+        await graceful_exit(0 if result.successful else 1)
     else:
         logger.debug("Closing")
-        exit(0)
+        await graceful_exit(0)
