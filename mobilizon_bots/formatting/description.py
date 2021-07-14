c4c046b2011f4279956233d98af05a489d5b1c46
Simone Robutti <simone.robutti@protonmail.com>
2021-07-14 12:25:58 +0200

    added format to markdown

---
+++ mobilizon_bots/formatting/description.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from bs4 import BeautifulSoup, Tag
+import markdownify
+
+
+def get_bottom_paragraphs(soup: BeautifulSoup) -> List[Tag]:
+    return [d for d in soup.findAll("p") if not d.find("p")]
+
+
+def html_to_plaintext(content):
+    soup = BeautifulSoup(content)
+    return "\n".join(
+        " ".join(tag.stripped_strings) for tag in get_bottom_paragraphs(soup)
+    )
+
+
+def html_to_markdown(content):
+    markdown = markdownify.markdownify(content)
+    escaped_markdown = markdown.replace(">", "\\>")
+    return escaped_markdown
---
+++ tests/formatting/test_parse.py
@@ -0,0 +1,20 @@
+import pytest
+
+from mobilizon_bots.formatting.description import html_to_plaintext
+
+
+@pytest.mark.parametrize(
+    "description, expected_output",
+    [
+        ["", ""],
+        ["<p>Description</p>", "Description"],
+        [
+            "<p>Some description <em>abc</em></p><p></p><p><strong>Bold</strong></p><p></p>"
+            "<p><em>Italic</em></p><p></p><blockquote><p>Quote</p></blockquote>",
+            "Some description abc\n\nBold\n\nItalic\n\nQuote",
+        ],
+        ["<p><a href='https://some_link.com'>Some Link</a></p>", "Some Link"],
+    ],
+)
+def test_html_to_plaintext(description, expected_output):
+    assert html_to_plaintext(description) == expected_output
