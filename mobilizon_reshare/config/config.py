127f0aa6a9b8acacd94011cbca4870b14afa1ab5
Giacomo Leidi <goodoldpaul@autistici.org>
2021-08-29 22:15:20 +0200

    settings.toml: Move secrets to dedicated file.
    Configuration files are checked in this order:
    
          1. CLI argument
          2. `$MOBILIZION_RESHARE_SETTINGS_FILE` environment variable;
          3. User configuration directory. On Linux that's `$XDG_CONFIG_HOME/mobilizon_reshare/<mobilizon-reshare-version>`;
          4. System configuration directory. On Linux that's the first element in `$XDG_CONFIG_DIRS` + `/mobilizon_reshare/<mobilizon-reshare-version>`.
          5. The default configuration distributed with the package.
    
    The first available configuration file will be loaded. Secrets can be set either
    via environment variables (such as `export MOBILIZON_RESHARE_PUBLISHER_TELEGRAM_TOKEN="my-telegram-token"` )
    or by setting the `$SECRETS_FOR_DYNACONF` environment variable to a `secrets.(toml|yaml|ini|json)` file.

--- mobilizon_reshare/config/config.py
+++ mobilizon_reshare/config/config.py
@@ -1,27 +1,79 @@
+import importlib.resources
 import os
-from typing import List
+from pathlib import Path
+from typing import Optional
 
+from appdirs import AppDirs
 from dynaconf import Dynaconf, Validator
 
+import mobilizon_reshare
 from mobilizon_reshare.config import strategies, publishers, notifiers
 from mobilizon_reshare.config.notifiers import notifier_names
 
 from mobilizon_reshare.config.publishers import publisher_names
 
+base_validators = [
+    # strategy to decide events to publish
+    Validator("selection.strategy", must_exist=True, is_type_of=str),
+    Validator(
+        "publishing.window.begin",
+        must_exist=True,
+        is_type_of=int,
+        gte=0,
+        lte=24,
+    ),
+    Validator("publishing.window.end", must_exist=True, is_type_of=int, gte=0, lte=24),
+    # url of the main Mobilizon instance to download events from
+    Validator("source.mobilizon.url", must_exist=True, is_type_of=str),
+    Validator("source.mobilizon.group", must_exist=True, is_type_of=str),
+]
+
+activeness_validators = [
+    Validator(f"publisher.{publisher_name}.active", must_exist=True, is_type_of=bool)
+    for publisher_name in publisher_names
+] + [
+    Validator(f"notifier.{notifier_name}.active", must_exist=True, is_type_of=bool)
+    for notifier_name in notifier_names
+]
+
+
+def current_version() -> str:
+    with importlib.resources.open_text(mobilizon_reshare, "VERSION") as fp:
+        return fp.read()
+
 
 def build_settings(
-    settings_files: List[str] = None, validators: List[Validator] = None
+    settings_file: Optional[str] = None, validators: Optional[list[Validator]] = None
 ):
-    SETTINGS_FILE = (
-        settings_files
-        or os.environ.get("MOBILIZION_RESHARE_SETTINGS_FILE")
-        or [
-            "mobilizon_reshare/settings.toml",
-            "mobilizon_reshare/.secrets.toml",
-            "/etc/mobilizon_reshare.toml",
-            "/etc/mobilizon_reshare_secrets.toml",
-        ]
-    )
+    """
+    Creates a Dynaconf base object. Configuration files are checked in this order:
+
+      1. CLI argument
+      2. `MOBILIZION_RESHARE_SETTINGS_FILE` environment variable;
+      3. User configuration directory. On Linux that's `$XDG_CONFIG_HOME/mobilizon_reshare/<mobilizon-reshare-version>`;
+      4. User configuration directory. On Linux that's the first element of
+         `$XDG_CONFIG_DIRS` + `/mobilizon_reshare/<mobilizon-reshare-version>`.
+      5. The default configuration distributed with the package.
+
+    The first available configuration file will be loaded.
+    """
+    dirs = AppDirs(appname="mobilizon-reshare", version=current_version())
+    with importlib.resources.path(
+        mobilizon_reshare, "settings.toml"
+    ) as bundled_settings_path:
+        SETTINGS_FILE = (
+            [
+                bundled_settings_path,
+                Path(dirs.site_config_dir, "mobilizon_reshare.toml"),
+                Path(dirs.user_config_dir, "mobilizon_reshare.toml"),
+                os.environ.get("MOBILIZION_RESHARE_SETTINGS_FILE"),
+                settings_file,
+            ]
+            # FIXME: This is needed because otherwise dynaconf would load the bundled settings.toml file.
+            if os.environ.get("ENV_FOR_DYNACONF", "") != "testing"
+            else [settings_file]
+        )
+
     ENVVAR_PREFIX = "MOBILIZON_RESHARE"
 
     return Dynaconf(
@@ -32,51 +84,17 @@ def build_settings(
     )
 
 
-def build_and_validate_settings(settings_files: List[str] = None):
+def build_and_validate_settings(settings_file: Optional[str] = None):
     """
     Creates a settings object to be used in the application. It collects and apply generic validators and validators
     specific for each publisher, notifier and publication strategy.
     """
 
-    preliminary_validators = (
-        [Validator("selection.strategy", must_exist=True, is_type_of=str)]
-        + [
-            Validator(
-                f"publisher.{publisher_name}.active", must_exist=True, is_type_of=bool
-            )
-            for publisher_name in publisher_names
-        ]
-        + [
-            Validator(
-                f"notifier.{notifier_name}.active", must_exist=True, is_type_of=bool
-            )
-            for notifier_name in notifier_names
-        ]
-    )
-
     # we first do a preliminary load of the settings without validation. We will later use them to determine which
     # publishers, notifiers and strategy have been selected
     raw_settings = build_settings(
-        settings_files=settings_files, validators=preliminary_validators
+        settings_file=settings_file, validators=activeness_validators
     )
-
-    # These validators are always applied
-    base_validators = [
-        # strategy to decide events to publish
-        Validator(
-            "publishing.window.begin",
-            must_exist=True,
-            is_type_of=int,
-            gte=0,
-            lte=24,
-        ),
-        Validator(
-            "publishing.window.end", must_exist=True, is_type_of=int, gte=0, lte=24
-        ),
-        # url of the main Mobilizon instance to download events from
-        Validator("source.mobilizon.url", must_exist=True, is_type_of=str),
-        Validator("source.mobilizon.group", must_exist=True, is_type_of=str),
-    ] + preliminary_validators
 
     # we retrieve validators that are conditional. Each module will analyze the settings and decide which validators
     # need to be applied.
@@ -86,7 +104,7 @@ def build_and_validate_settings(settings_files: List[str] = None):
 
     # we rebuild the settings, providing all the selected validators.
     settings = build_settings(
-        settings_files,
+        settings_file,
         base_validators
         + strategy_validators
         + publisher_validators
@@ -106,21 +124,21 @@ def build_and_validate_settings(settings_files: List[str] = None):
 class CustomConfig:
     _instance = None
 
-    def __new__(cls, settings_files: List[str] = None):
+    def __new__(cls, settings_file: Optional[str] = None):
         if cls._instance is None:
             cls._instance = super(CustomConfig, cls).__new__(cls)
-            cls.settings = build_and_validate_settings(settings_files)
+            cls.settings = build_and_validate_settings(settings_file)
         return cls._instance
 
-    def update(self, settings_files: List[str] = None):
-        self.settings = build_and_validate_settings(settings_files)
+    def update(self, settings_file: Optional[str] = None):
+        self.settings = build_and_validate_settings(settings_file)
 
 
-def get_settings():
-    config = CustomConfig()
+def get_settings(settings_file: Optional[str] = None):
+    config = CustomConfig(settings_file)
     return config.settings
 
 
-def update_settings_files(settings_files: List[str] = None):
-    CustomConfig().update(settings_files)
+def update_settings_files(settings_file: Optional[str] = None):
+    CustomConfig().update(settings_file)
     return CustomConfig().settings
--- tests/config/test_validation.py
+++ tests/config/test_validation.py
@@ -1,3 +1,5 @@
+import os
+
 import dynaconf
 import pkg_resources
 import pytest
@@ -15,26 +17,51 @@ def invalid_settings_file(tmp_path, toml_content):
 @pytest.mark.parametrize("toml_content", ["invalid toml["])
 def test_update_failure_invalid_toml(invalid_settings_file):
     with pytest.raises(dynaconf.vendor.toml.decoder.TomlDecodeError):
-        update_settings_files([invalid_settings_file.absolute()])
+        update_settings_files(invalid_settings_file.absolute())
 
 
 @pytest.mark.parametrize("toml_content", [""])
 def test_update_failure_invalid_preliminary_config(invalid_settings_file):
+    os.environ["SECRETS_FOR_DYNACONF"] = ""
+
     with pytest.raises(dynaconf.validator.ValidationError):
-        update_settings_files([invalid_settings_file.absolute()])
+        update_settings_files(invalid_settings_file.absolute())
 
 
 @pytest.mark.parametrize(
     "invalid_toml,pattern_in_exception",
     [
-        ["config_with_strategy.toml", "publisher.*.active"],
+        ["config_with_invalid_strategy.toml", "break_between_events_in_minutes"],
+        ["config_with_invalid_mobilizon.toml", "mobilizon"],
         ["config_with_preliminary.toml", "publishing.window.begin"],
+    ],
+)
+def test_update_failure_config_base_validators(invalid_toml, pattern_in_exception):
+
+    with pytest.raises(dynaconf.validator.ValidationError) as e:
+        update_settings_files(
+            pkg_resources.resource_filename("tests.resources.config", invalid_toml)
+        )
+    assert e.match(pattern_in_exception)
+
+
+@pytest.mark.parametrize(
+    "invalid_toml,pattern_in_exception",
+    [
+        ["empty.toml", "publisher.*.active"],
         ["config_with_invalid_telegram.toml", "token"],
     ],
 )
-def test_update_failure_config_without_publishers(invalid_toml, pattern_in_exception):
+def test_update_failure_config_all_validators(invalid_toml, pattern_in_exception):
+
+    os.environ["SECRETS_FOR_DYNACONF"] = pkg_resources.resource_filename(
+        "tests.resources.config", invalid_toml
+    )
+
     with pytest.raises(dynaconf.validator.ValidationError) as e:
         update_settings_files(
-            [pkg_resources.resource_filename("tests.resources.config", invalid_toml)]
+            pkg_resources.resource_filename(
+                "tests.resources.config", "test_singleton.toml"
+            )
         )
     assert e.match(pattern_in_exception)
