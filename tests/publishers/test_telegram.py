522d4736bc91169cbaa5b38e1be5bb2e73f2b3ee
Simone Robutti <simone.robutti@protonmail.com>
2021-09-30 17:59:43 +0200

    added telegram response validation

--- tests/publishers/test_telegram.py
+++ tests/publishers/test_telegram.py
@@ -1,7 +1,11 @@
 import pytest
+import requests
 
-from mobilizon_reshare.publishers.exceptions import InvalidEvent
-from mobilizon_reshare.publishers.platforms.telegram import TelegramFormatter
+from mobilizon_reshare.publishers.exceptions import InvalidEvent, InvalidResponse
+from mobilizon_reshare.publishers.platforms.telegram import (
+    TelegramFormatter,
+    TelegramPublisher,
+)
 
 
 def test_message_length_success(event):
@@ -36,3 +40,42 @@ def test_event_validation(event):
     event.description = None
     with pytest.raises(InvalidEvent):
         TelegramFormatter().validate_event(event)
+
+
+def test_validate_response():
+    response = requests.Response()
+    response.status_code = 200
+    response._content = b"""{"ok":true}"""
+    TelegramPublisher()._validate_response(response)
+
+
+def test_validate_response_invalid_json():
+    response = requests.Response()
+    response.status_code = 200
+    response._content = b"""{"osxsa"""
+    with pytest.raises(InvalidResponse) as e:
+        TelegramPublisher()._validate_response(response)
+
+    e.match("json")
+
+
+def test_validate_response_invalid_request():
+    response = requests.Response()
+    response.status_code = 400
+    response._content = b"""{"error":true}"""
+    with pytest.raises(InvalidResponse) as e:
+
+        TelegramPublisher()._validate_response(response)
+
+    e.match("Server returned invalid data")
+
+
+def test_validate_response_invalid_response():
+    response = requests.Response()
+    response.status_code = 200
+    response._content = b"""{"error":true}"""
+    with pytest.raises(InvalidResponse) as e:
+
+        TelegramPublisher()._validate_response(response)
+
+    e.match("Invalid request")
