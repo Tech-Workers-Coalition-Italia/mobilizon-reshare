fb51b2bd61fe782e382892e2578c4a252c7d2111
Simone Robutti <simone.robutti@protonmail.com>
2021-10-21 10:15:52 +0200

    improved format

--- mobilizon_reshare/cli/cli.py
+++ mobilizon_reshare/cli/cli.py
@@ -10,6 +10,7 @@ from mobilizon_reshare.cli.commands.format.format import format_event
 from mobilizon_reshare.cli.commands.inspect.inspect_event import inspect_events
 from mobilizon_reshare.cli.commands.start.main import main as start_main
 from mobilizon_reshare.cli.commands.recap.main import main as recap_main
+from mobilizon_reshare.config.publishers import publisher_names
 
 from mobilizon_reshare.event.event import EventPublicationStatus
 
@@ -89,11 +90,11 @@ def inspect(ctx, status, begin, end, settings_file):
 
 @mobilizon_reshare.command(
     help="Format and print event with mobilizon id EVENT-ID using the publisher's format named"
-    "PUBLISHER (i.e.:'telegram','twitter', ...)"
+    "PUBLISHER"
 )
 @settings_file_option
-@click.argument("event-id", type=str)
-@click.argument("publisher", type=str)
+@click.argument("event-id", type=click.UUID)
+@click.argument("publisher", type=click.Choice(publisher_names))
 def format(settings_file, event_id, publisher):
     safe_execution(
         functools.partial(format_event, event_id, publisher), settings_file,
