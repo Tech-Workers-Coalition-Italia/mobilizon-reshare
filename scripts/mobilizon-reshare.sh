d01bb2f3e0e1d6326aac6c837c724c7a9fa722a5
Giacomo Leidi <goodoldpaul@autistici.org>
2021-11-27 23:29:00 +0100

    scripts: mobilizon-reshare.sh: Use poetry.

--- scripts/mobilizon-reshare.sh
+++ scripts/mobilizon-reshare.sh
@@ -2,9 +2,8 @@
 
 export MOBILIZON_RESHARE_LOG_DIR="/tmp"
 export MOBILIZON_RESHARE_LOCAL_STATE_DIR="/tmp"
-export PYTHONPATH="$(pwd):${PYTHONPATH}"
 export SECRETS_FOR_DYNACONF="$(pwd)/.secrets.toml"
 export SETTINGS_FILE_FOR_DYNACONF="$(pwd)/mobilizon_reshare.toml"
 export ENV_FOR_DYNACONF="production"
 
-python "$(pwd)/mobilizon_reshare/cli/cli.py" "$@"
+poetry run mobilizon-reshare "$@"
