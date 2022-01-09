587b759cf3e9337197122471f2f1492792b796d4
Giacomo Leidi <goodoldpaul@autistici.org>
2022-01-09 04:10:31 +0100

    docker: Upstream python-markdownify.

--- channels-lock.scm
+++ channels-lock.scm
@@ -5,7 +5,7 @@
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (commit
-   "01ec5efff7cf95cae69493bd4ccfdd21a38770ae")
+   "195bb1fb9d55d8e5187d669c63a3cde747fc5f64")
   (introduction
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
