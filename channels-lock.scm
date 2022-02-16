9c44c8d730dfd092b96320cc7a7dcf346091c3e8
Giacomo Leidi <goodoldpaul@autistici.org>
2022-02-16 12:23:31 +0100

    Enforce the same dependencies in dev and prod. (#141)
    This patch restores Guix' full sanity check, which runs the CLI
    entrypoint to verify all required dependencies are present.

--- channels-lock.scm
+++ channels-lock.scm
@@ -8,6 +8,6 @@
    "1121fa432f0e6422d5f9ebb96fb0014c4d5231f5")
   (introduction
    (make-channel-introduction
-    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    "41000d16c5c1586482a76d856c3152a6b8fcce8a"
     (openpgp-fingerprint
      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
