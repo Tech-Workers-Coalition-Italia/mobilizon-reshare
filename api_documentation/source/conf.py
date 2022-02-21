18ee8e5585f29bd0976920936d0eaf4c93ab3dfc
magowiz <magowiz@gmail.com>
2022-02-21 18:11:27 +0100

    remove module path prefix from methods name

--- api_documentation/source/conf.py
+++ api_documentation/source/conf.py
@@ -49,3 +49,6 @@ html_theme = "sphinx_material"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# disable full module path in methods list
+add_module_names = False
