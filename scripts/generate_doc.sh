c2a5b86070f568fe5d225fc385bbc5912d516ba5
magowiz <magowiz@gmail.com>
2022-02-22 18:42:40 +0100

    remove gui browser documentation open, use /bin/sh instead of /bin/bash

--- scripts/generate_doc.sh
+++ scripts/generate_doc.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 export SECRETS_FOR_DYNACONF='mobilizon_reshare/.secrets.toml'
 poetry run sphinx-apidoc -f -o api_documentation/source/ mobilizon_reshare/
 cd api_documentation 
 poetry run make html
-if [ -z $1 ] || [[ $1 != 'nox' ]]
-then
-#	xdg-open build/html/index.html
-	firefox build/html/index.html
-fi
 cd ..
