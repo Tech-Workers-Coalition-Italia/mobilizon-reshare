0142d8dd7425bcb92201537cf67d147744e419a8
Giacomo Leidi <goodoldpaul@autistici.org>
2022-05-17 01:57:12 +0200

    Move publishing of events and publications to retry.

--- mobilizon_reshare/cli/commands/publish/main.py
+++ mobilizon_reshare/cli/commands/publish/main.py
@@ -1,33 +1,14 @@
 import logging
 
-from mobilizon_reshare.main.publish import (
-    select_and_publish,
-    publish_event,
-    publish_publications,
-)
-from mobilizon_reshare.storage.query.read import get_mobilizon_event, get_publication
+from mobilizon_reshare.main.publish import select_and_publish
 
 logger = logging.getLogger(__name__)
 
 
-async def publish_command(event, publication, platform):
+async def publish_command():
     """
-    If no arguments are provided, select an event with the current configured strategy
+    Select an event with the current configured strategy
     and publish it to all active platforms.
-
-    - If list_platforms is True print all platforms and exit
-    - Otherwise:
-       + If publication is not None, publish it.
-       + Otherwise if event is not None publish it.
     """
-    if publication and event:
-        logger.error("-P/--publication and -E/--event are incompatible!")
-        return 1
-
-    if publication:
-        report = await publish_publications(await get_publication(publication))
-    elif event:
-        report = await publish_event(await get_mobilizon_event(event), [platform])
-    else:
-        report = await select_and_publish()
+    report = await select_and_publish()
     return 0 if report and report.successful else 1
--- mobilizon_reshare/main/retry.py
+++ mobilizon_reshare/main/retry.py
@@ -2,22 +2,20 @@ import logging
 from typing import Optional
 from uuid import UUID
 
+from mobilizon_reshare.main.publish import publish_publications
 from mobilizon_reshare.publishers.coordinator import (
-    PublisherCoordinator,
     PublisherCoordinatorReport,
-    PublicationFailureLoggerCoordinator,
 )
 from mobilizon_reshare.storage.query.exceptions import EventNotFound
 from mobilizon_reshare.storage.query.read import (
     get_failed_publications_for_event,
     get_publication,
 )
-from mobilizon_reshare.storage.query.write import save_publication_report
 
 logger = logging.getLogger(__name__)
 
 
-async def retry_event_publications(event_id):
+async def retry_event_publications(event_id) -> Optional[PublisherCoordinatorReport]:
 
     failed_publications = await get_failed_publications_for_event(event_id)
     if not failed_publications:
@@ -25,24 +23,17 @@ async def retry_event_publications(event_id):
         return
 
     logger.info(f"Found {len(failed_publications)} publications.")
-    return PublisherCoordinator(failed_publications).run()
+    return await publish_publications(failed_publications)
 
 
 async def retry_publication(publication_id) -> Optional[PublisherCoordinatorReport]:
-    # TODO test this function
     publication = await get_publication(publication_id)
     if not publication:
         logger.info(f"Publication {publication_id} not found.")
         return
 
     logger.info(f"Publication {publication_id} found.")
-    reports = PublisherCoordinator([publication]).run()
-
-    await save_publication_report(reports)
-
-    for report in reports.reports:
-        if not report.succesful:
-            PublicationFailureLoggerCoordinator(report,).notify_failure()
+    return await publish_publications([publication])
 
 
 async def retry_event(
@@ -53,15 +44,8 @@ async def retry_event(
             "Autonomous retry not implemented yet, please specify an event_id"
         )
     try:
-        reports = await retry_event_publications(mobilizon_event_id)
+        return await retry_event_publications(mobilizon_event_id)
     except EventNotFound as e:
         logger.debug(e, exc_info=True)
         logger.error(f"Event with id {mobilizon_event_id} not found")
         return
-
-    if not reports:
-        return
-    await save_publication_report(reports)
-    for report in reports.reports:
-        if not report.succesful:
-            PublicationFailureLoggerCoordinator(report,).notify_failure()
