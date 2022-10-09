fb952729ec5d29d230b96960a1f6368ea40b0f25
Simone Robutti <simone.robutti@protonmail.com>
2022-10-09 16:29:48 +0200

    waiting for postgress in script

--- scripts/generate_aerich_migrations.sh
+++ scripts/generate_aerich_migrations.sh
@@ -10,7 +10,6 @@ get_abs_filename() {
 
 PROJECT_DIR="$(get_abs_filename $0)/.."
 
-
 cleanup() {
   # cleaning sqlite db
   echo "Removing /tmp/foo"
@@ -31,7 +30,7 @@ poetry install
 . "$(poetry env info -p)/bin/activate"
 
 # I create a new SQLite db to run the migrations and generate a new one
-echo("Generating SQLite migrations")
+echo "Generating SQLite migrations"
 export DYNACONF_DB_URL="sqlite:///tmp/tmp.db"
 cd "$PROJECT_DIR/mobilizon_reshare/migrations/sqlite/"
 
@@ -41,14 +40,21 @@ aerich migrate
 # I use a dedicated docker-compose file to spin up a postgres instance, connect to it, run the migrations and generate a
 # new one
 
-echo("Generating postgres migrations")
+echo "Generating postgres migrations"
 export DYNACONF_DB_URL="postgres://mobilizon_reshare:mobilizon_reshare@localhost:5432/mobilizon_reshare"
 cd $PROJECT_DIR
 
 docker-compose -f docker-compose-migration.yml up -d
 
 cd "$PROJECT_DIR/mobilizon_reshare/migrations/postgres/"
+until [ "$(docker inspect mo-re_db_1 --format='{{json .State.Health.Status}}')" = "\"healthy\"" ];
+do
+  echo "Waiting for postgres"
+  if [ "$(docker inspect mo-re_db_1 --format='{{json .State.Health.Status}}')" = "\"healthy\"" ]
+  then
+    break
+  fi
+  sleep 1s
+done
 aerich upgrade
 aerich migrate
-
-cleanup
