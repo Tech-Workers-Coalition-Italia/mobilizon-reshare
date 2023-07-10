9744f436ae2062a9cb7c190aacf453fa11fcf829
Giacomo Leidi <goodoldpaul@autistici.org>
2023-07-11 01:50:19 +0200

    Update build_docker_image.sh.

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -3,4 +3,4 @@ set -eu
 
 guix time-machine -C channels-lock.scm -- build -f guix.scm
 
-guix time-machine -C channels-lock.scm -- pack -L . -f docker -S /opt/bin=bin --save-provenance --root=docker-image.tar.gz --entry-point=bin/scheduler.py mobilizon-reshare-scheduler
+guix time-machine -C channels-lock.scm -- pack -L . -f docker -S /opt/bin=bin --save-provenance --root=docker-image.tar.gz --entry-point=bin/scheduler.py mobilizon-reshare-scheduler python
