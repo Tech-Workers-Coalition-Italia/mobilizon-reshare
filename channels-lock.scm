0f19cf4a9e531057134701e6d830a1132db211f9
Giacomo Leidi <goodoldpaul@autistici.org>
2023-10-30 20:57:07 +0100

    Revert "temporary lock"
    This reverts commit 8d3026523aad41415c97323a23619e6edfe09690.

--- channels-lock.scm
+++ channels-lock.scm
@@ -10,7 +10,7 @@
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (commit
-   "53fd3989d3b99f6eba4a5a007a578f1d9b8e0b61")
+   "79a3cd34c0318928186a04b6481c4d22c0051d04")
   (introduction
    (make-channel-introduction
     "afb9f2752315f131e4ddd44eba02eed403365085"
