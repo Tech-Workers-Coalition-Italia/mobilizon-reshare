be715d201c989380bf12793d477547d220807f27
Giacomo Leidi <goodoldpaul@autistici.org>
2024-03-01 23:38:11 +0100

    Update dependencies.

--- channels-lock.scm
+++ channels-lock.scm
@@ -10,7 +10,7 @@
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
   (commit
-   "23283c50e96d7812a42b1272329fa3ba05a5a6e6")
+   "9038a46751c5090246e64b63ff752064833c435b")
   (introduction
    (make-channel-introduction
     "afb9f2752315f131e4ddd44eba02eed403365085"
--- guix.scm
+++ guix.scm
@@ -5,6 +5,7 @@
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages markup)     ;; for python-markdownify
+  #:use-module (gnu packages python-web) ;; for python-fastapi-pagination-minimal
   #:use-module (gnu packages python-xyz) ;; for python-apscheduler
   #:use-module (mobilizon-reshare package)
   #:use-module (mobilizon-reshare dependencies)
@@ -32,6 +33,7 @@
       (source mobilizon-reshare-git-origin)
       (propagated-inputs
        (modify-inputs (package-propagated-inputs mobilizon-reshare)
+         (replace "python-fastapi-pagination-minimal" python-fastapi-pagination-minimal)
          (replace "python-markdownify" python-markdownify))))))
 
 (define-public mobilizon-reshare-scheduler
